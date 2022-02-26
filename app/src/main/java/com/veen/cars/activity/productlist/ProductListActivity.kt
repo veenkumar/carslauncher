5d47b6ce13d6fe5260ce3ca1fa4177afbcef5be7
veenkumar <praveen.kumar@mobulous.com>
2022-02-26 09:12:41 +0530

    Sales and Service work

--- app/src/main/java/com/veen/cars/activity/productlist/ProductListActivity.kt
+++ app/src/main/java/com/veen/cars/activity/productlist/ProductListActivity.kt
@@ -2,11 +2,15 @@ package com.veen.cars.activity.productlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.veen.cars.R
+import com.veen.cars.databinding.ActivityProductListBinding
 
 class ProductListActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityProductListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_list)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_product_list)
     }
 }
--- app/src/main/java/com/veen/cars/activity/salesandservice/SalesAndServiceActivity.kt
+++ app/src/main/java/com/veen/cars/activity/salesandservice/SalesAndServiceActivity.kt
@@ -2,11 +2,15 @@ package com.veen.cars.activity.salesandservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.veen.cars.R
+import com.veen.cars.databinding.ActivitySalesAndServiceBinding
 
 class SalesAndServiceActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySalesAndServiceBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sales_and_service)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sales_and_service)
     }
 }
