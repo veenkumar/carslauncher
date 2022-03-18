e5bb9e3989ecaa2045c8ffe11cedaf358ccbe6eb
veenkumar <praveen.kumar@mobulous.com>
2022-03-18 21:54:08 +0530

    changes day2

--- app/src/main/java/com/veen/cars/activity/salesandservice/SalesAndServiceActivity.kt
+++ app/src/main/java/com/veen/cars/activity/salesandservice/SalesAndServiceActivity.kt
@@ -1,32 +1,46 @@
 package com.veen.cars.activity.salesandservice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.setPadding
 import androidx.databinding.DataBindingUtil
 import com.veen.cars.R
 import com.veen.cars.databinding.ActivitySalesAndServiceBinding
 
 class SalesAndServiceActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySalesAndServiceBinding
+    private var chooseType = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_sales_and_service)
 
-        binding.enquiry.setOnClickListener {
-            if (binding.enquiryLayout.visibility == View.GONE){
-                binding.enquiryLayout.visibility = View.VISIBLE
-            } else {
-                binding.enquiryLayout.visibility = View.GONE
-            }
+        binding.ssSales.setOnClickListener {
+            chooseType = "1"
+            getServiceType(chooseType)
         }
 
-        binding.serviceMain.setOnClickListener {
-            if (binding.serviceLayout.visibility == View.GONE){
-                binding.serviceLayout.visibility = View.VISIBLE
-            } else {
-                binding.serviceLayout.visibility = View.GONE
+        binding.ssServices.setOnClickListener {
+            chooseType = "2"
+            getServiceType(chooseType)
+        }
+    }
+
+    private fun getServiceType(type: String) {
+        when(type){
+            "1"->{
+                binding.ssSales.setBackgroundResource(R.drawable.greybutton)
+                binding.ssSales.setTextColor(getColor(R.color.white))
+
+                binding.ssServices.setBackgroundResource(R.drawable.lightwhitebutton)
+                binding.ssServices.setTextColor(getColor(R.color.black))
+            }
+            "2"->{
+                binding.ssServices.setBackgroundResource(R.drawable.greybutton)
+                binding.ssServices.setTextColor(getColor(R.color.white))
+
+                binding.ssSales.setBackgroundResource(R.drawable.lightwhitebutton)
+                binding.ssSales.setTextColor(getColor(R.color.black))
             }
         }
     }
